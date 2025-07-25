f032a4274634a9e1de5f74bba1d03cadbd763d36
Rajesh Kumar Str <vikoshiya.rajeshkumarstr@gmail.com>
2025-07-25 14:32:38 +0530

    Initial commit

--- api/send.js
+++ api/send.js
@@ -1,6 +1,6 @@
 const nodemailer = require("nodemailer");
 
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
@@ -61,4 +61,4 @@ export default async function handler(req, res) {
       error: 'Failed to send email. Please try again later.' 
     });
   }
-}
+};
